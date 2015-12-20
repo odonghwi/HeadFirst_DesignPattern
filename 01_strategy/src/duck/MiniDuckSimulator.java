5194d1521d9924cd66bef8124139604f28cbe174
odonghwi <odonghwi@gmail.com>
2015-12-20 20:12:45 +0900

    01_strategy_pattern

--- 01_strategy/src/duck/MiniDuckSimulator.java
+++ 01_strategy/src/duck/MiniDuckSimulator.java
@@ -5,11 +5,13 @@ import duck.ducks.MallardDuck;
 
 public class MiniDuckSimulator {
 
-    public static void main(String[] args)
+
+    public static void main(String args[])
     {
         Duck mallard = new MallardDuck();
         mallard.performQuack();
         mallard.performFly();
         mallard.display();
+        mallard.swim();
     }
 }
